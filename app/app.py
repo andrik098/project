864bfd72b96a866df43278eed9fe8858596275da
andrik098 <163187209+andrik098@users.noreply.github.com>
2024-08-18 17:34:41 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -28,11 +28,6 @@ app.include_router(
     tags=["auth"],
 )
 app.include_router(
-    fastapi_users.get_reset_password_router(),
-    prefix="/auth",
-    tags=["auth"],
-)
-app.include_router(
     fastapi_users.get_verify_router(UserRead),
     prefix="/auth",
     tags=["auth"],
